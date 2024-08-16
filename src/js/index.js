70791ad216807f598d90a5b017e4d81038f96561
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-08-16 10:05:17 +0800

    save last theme in localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,19 @@
 const body = document.body;
 const btnTheme = document.getElementById("btn-theme");
 
+if (localStorage.getItem("darkTheme")) {
+    body.classList.add("dark");
+    btnTheme.className = "ri-moon-fill";
+}
+
 btnTheme.addEventListener("click", function () {
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
         btnTheme.className = "ri-sun-fill";
+        localStorage.removeItem("darkTheme");
     } else {
         body.classList.add("dark");
         btnTheme.className = "ri-moon-fill";
+        localStorage.setItem("darkTheme", true);
     }
 });
